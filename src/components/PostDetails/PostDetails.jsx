b751a32ea80fce85f1513cdc339e221d42bade10
Cody <ayanwunmiabdulroheem@gmail.com>
2022-09-14 12:34:00 +0100

    routing bug has been fixed

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react'
-import {Box} from '@chakra-ui/react'
+import {Box, VStack, Text, Button, Image, Heading, Container} from '@chakra-ui/react'
+import {ExternalLinkIcon} from '@chakra-ui/icons'
 import {useParams} from 'react-router-dom'
 import {app} from '../../../firebaseConfig'
-import {getFirestore, collection, query, onSnapshot} from 'firebase/firestore'
+import {getFirestore, collection, query, onSnapshot, getDocs,} from 'firebase/firestore'
+import test from '../../assets/CODE.png'
 
 const PostDetails = () => {
   const [posts, setPosts] = useState([]);
-  // const [singlePost, setSinglePost] = useState([])
+
    //initialize firestore
   const db = getFirestore();
 
@@ -16,27 +18,35 @@ const PostDetails = () => {
   const {id} = useParams();
   console.log(id);
 
+  // 
+
   useEffect(() => {
-    onSnapshot(q, (snapshot) => {
-        let items = [];
-        snapshot.docs.map((doc) => {
-          items.push({...doc.data(), id: doc.id});
-          return setPosts(items)});
-      });
-
-      const newPost = posts.filter((item) => item.id === id);
-      console.log(newPost);
-      setPosts(newPost);
-
+    getDocs(colref).then((snapshot) => {
+      let items = [];
+      snapshot.docs.map((doc) => {
+        items.push({...doc.data(), id: doc.id})
+        const newPost = items.find((post) => post.id === id);
+        return setPosts(newPost);
+      })
+    })
+    //  onSnapshot(q, (snapshot) => {
+    //     let items = [];
+    //     snapshot.docs.map((doc) => {
+    //       items.push({...doc.data(), id: doc.id});
+    //       return setPosts(items)});
+    // });
   }, []); 
-
   console.log(posts);
 
     // const [newPost, setNewPost] = useState();
   return (
-    <div>
-      {/* {posts.find((post) => post.id === parseInt(id))} */}
-    </div>
+    <VStack>
+      <Image src={test} />
+      <Heading size={'md'} textAlign='center' mt='5'>{posts.post_title}</Heading>
+      <Text>{posts.author_name && posts.author_name}</Text>
+      <Text>Category: <ExternalLinkIcon /> {posts.post_category}</Text>
+      <Text p='5' textAlign={'justify'}>{posts.post_content}</Text>
+    </VStack>
   )
 }
 
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
-import {Box, Heading, Text, FormControl, FormLabel, Input, Button, Stack, useToast, Flex, Container, HStack} from '@chakra-ui/react'
+import {Box, Heading, Text, FormControl, FormLabel, Input, Button, Stack, useToast, Flex, Container, Avatar} from '@chakra-ui/react'
 import {app} from '../../../firebaseConfig'
 import {getAuth, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword} from 'firebase/auth'
 import {useNavigate, Link} from 'react-router-dom'
+import {getStorage, uploadBytes, ref} from 'firebase/storage'
+import {v4} from 'uuid'
 
 const Signup = () => {
     const [userName, setUserName] = useState('')
@@ -10,20 +12,30 @@ const Signup = () => {
     const [password, setPassword] = useState('');
     const toast = useToast();
     const navigate = useNavigate();
+    const [avatar, setAvatar] = useState(null);
 
     const auth = getAuth();
+    const storage = getStorage();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(userName && email && password){
+        if(userName && email && password && avatar){
+
+            const avatarRef = ref(storage, `avatar/${avatar.name + v4()}`);
+            uploadBytes(avatarRef, avatar).then(() => {
+                console.log('avatar uploaded');
+            });
+
             createUserWithEmailAndPassword(auth, email, password).then((cred) => {
                 console.log('user created', cred.user);
+                
                 toast({
                     title: 'Account Created', 
                     description: 'Your account has been created successfully',
                     status: 'success', duration: '5000',
                     isClosable: 'true', 
                     position: 'top-right'});
+
                 setEmail('');
                 setPassword('');
                 setUserName('');
@@ -33,6 +45,7 @@ const Signup = () => {
             }).catch((err) => {
                 console.log(err.message);
             })
+
         }else{
              toast({
                     title: 'Invalid Credentials', 
@@ -48,16 +61,28 @@ const Signup = () => {
           <Flex textAlign='center' justifyContent={'center'} spacing='-0.95' mb='3' borderRadius={'xl'}>
                 <Heading size={'xl'} >Eco</Heading><Heading color='whatsapp.600'size={'xl'}>Scribes</Heading>
           </Flex>
-    <Stack spacing='4' maxW={'450px'} margin='auto' bg='gray.50' boxShadow='lg' p='5' pt='2'>
+    <Stack spacing='4' maxW={'450px'} margin='auto' bg='gray.50' boxShadow='lg' p='5' pt='2' mb='5'>
         <Heading textAlign='center' mb='4' size={'lg'} mt='6'>Signup</Heading>
         <FormControl maxWidth='450px' textAlign={'left'} isRequired>
+
+            <Flex justify={'center'} mb='5'>
+                <Avatar src='' name={userName} size='xl' />
+            </Flex>
+
+            <FormLabel>Select your avatar</FormLabel>
+            <Input type='file' onChange={(e) => setAvatar(e.target.files[0])} mb='5'  maxWidth='450px'/>
+
             <FormLabel>Username</FormLabel>
             <Input type='text' placeholder='input your username' mb='5'  maxWidth='450px' value={userName} onChange={(e) => setUserName(e.target.value)} />
+
             <FormLabel>Email</FormLabel>
             <Input type='email' placeholder='input your email' mb='5'  maxWidth='450px' value={email} onChange={(e) => setEmail(e.target.value)} />
+
             <FormLabel>Password</FormLabel>
             <Input type='password' placeholder='input your password'  maxWidth='450px' mb='5' value={password} onChange={(e) => setPassword(e.target.value)} />
+
             <Button colorScheme='twitter' size='sm' type='submit' onClick={handleSubmit}>Signup</Button>
+
         </FormControl>
         <Text textAlign={'left'} >Already have an account? <Link to='/login'>Sign in</Link> </Text>
     </Stack>
